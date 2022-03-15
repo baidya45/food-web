8b3eff04295a13689cba585fe62b0e99f5627427
Baidyanath Dey <baidyadey11@gmail.com>
2022-03-15 17:07:45 +0530

    menu tab selection done

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const navToggler = document.getElementById('btn');
 const nav = document.querySelector('.nav');
+const menuTab = document.querySelectorAll(".menu-tab-items")
+const menuContent = document.querySelectorAll(".menu-tab-content")
+console.log(menuContent);
+
+menuTab[0].addEventListener('click',()=>{
+    menuTab[0].classList.add("active")
+    menuTab[1].classList.remove('active')
+    menuTab[2].classList.remove('active')
+    menuContent[0].classList.add("active2")
+    menuContent[1].classList.remove("active2")
+    menuContent[2].classList.remove("active2")
+})
+menuTab[1].addEventListener('click',()=>{
+    menuTab[1].classList.add("active")
+    menuTab[0].classList.remove('active')
+    menuTab[2].classList.remove('active')
+    menuContent[0].classList.remove("active2")
+    menuContent[1].classList.add("active2")
+    menuContent[2].classList.remove("active2")
+})
+menuTab[2].addEventListener('click',()=>{
+    menuTab[2].classList.add("active")
+    menuTab[1].classList.remove('active')
+    menuTab[0].classList.remove('active')
+    menuContent[0].classList.remove("active2")
+    menuContent[1].classList.remove("active2")
+    menuContent[2].classList.add("active2")
+})
 
 navToggler.addEventListener('click',toggle)
 
